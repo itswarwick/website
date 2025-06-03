5bc41fc92d442da9ec4e04d6d3c226b805eba007
itswarwick <113207666+itswarwick@users.noreply.github.com>
2025-06-03 10:27:07 -0700

    Duplicate config file removed. Added speedinsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Roboto_Mono } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -22,7 +23,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={`${robotoMono.variable}`}>
-      <body className={`${inter.className}`}>{children}</body>
+      <body className={`${inter.className}`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Navigation from '@/components/Navigation'
 import Hero from '@/components/Hero'
 import FeaturedProjects from '@/components/FeaturedProjects'
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   return (
